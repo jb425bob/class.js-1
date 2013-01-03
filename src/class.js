d3dc6173935b39839a721b8bcdf7d9aff5ffdeae
Matthew Strickland <matthew@redcrown.co>
2013-01-03 12:29:05 -0800

    Added a CID generator to each class instance

--- src/class.js
+++ src/class.js
@@ -1,9 +1,19 @@
 !function () {
 
+    function guid() {
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g,
+            function (c) {
+                var r = Math.random()*16|0
+                  , v = (c == "x") ? r : (r&0x3|0x8);
+                return v.toString(16);
+            }).toUpperCase();
+    }
+
     function ctor() {}
 
     function Class( parent ) {
         function klass() {
+            this.cid = guid();
             this.init.apply(this, arguments);
         }
 
--- test/tests.js
+++ test/tests.js
@@ -2,32 +2,51 @@ var expect = require("chai").expect;
 var Class  = require('../src/class.js');
 
 describe("Class", function () {
+
     it("should exist", function () {
         expect(Class).to.exist;
     })
 
-    it("should extend prototypes", function () {
-        var Person = new Class;
-        Person.include({
-            n: 5
-        })
-        var Person2 = new Class(Person);
-        var person2 = new Person2;
-        expect(person2.n).to.equal(5);
-    })
+    describe("when creating a class", function () {
 
-    describe("Create method", function () {
-
-        it("should return a new Class extending from the parent class", function () {
-            var Person  = new Class;
-            Person.include({n: 5});
-            var Person2 = Person.create({
-                getN: function () {
-                    return this.n;
-                }
-            });
+        it("should extend prototypes", function () {
+            var Person = new Class;
+            Person.include({
+                n: 5
+            })
+            var Person2 = new Class(Person);
             var person2 = new Person2;
-            expect(person2.getN()).to.equal(5);
+            expect(person2.n).to.equal(5);
         })
+
+        it("should add a unique CID GUID", function () {
+            var Person = new Class;
+            var person = new Person;
+
+            expect(person.cid.length).to.equal(36);
+            expect(person.cid.match(/-/g).length).to.equal(4);
+            expect(person.cid.match("4").length).to.be.above(0);
+        })
+
     })
+
+
+    describe("methods", function () {
+
+        describe("create method", function () {
+            it("should return a new Class extending from the parent class", function () {
+                var Person  = new Class;
+                Person.include({n: 5});
+                var Person2 = Person.create({
+                    getN: function () {
+                        return this.n;
+                    }
+                });
+                var person2 = new Person2;
+                expect(person2.getN()).to.equal(5);
+            })
+        })
+
+    })
+
 })
